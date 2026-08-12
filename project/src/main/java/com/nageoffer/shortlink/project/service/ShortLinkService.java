9cc81e2057a156317f8e84ccbd0d3ab11e5732dd
local git <2268952893@qq.com>
2026-08-12 11:07:51 +0800

    feature: 开发短链接跳转功能

--- project/src/main/java/com/nageoffer/shortlink/project/service/ShortLinkService.java
+++ project/src/main/java/com/nageoffer/shortlink/project/service/ShortLinkService.java
@@ -9,6 +9,8 @@ import com.nageoffer.shortlink.project.dto.req.ShortLinkUpdateReqDTO;
 import com.nageoffer.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
 import com.nageoffer.shortlink.project.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.nageoffer.shortlink.project.dto.resp.ShortLinkPageRespDTO;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 
 import java.util.List;
 
@@ -45,4 +47,11 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
     List<ShortLinkGroupCountQueryRespDTO> listGroupShortLinkCount(List<String> requestParam);
 
 
+    /**
+     * 恢复短链接
+     * @param shortUri 短链接后缀
+     * @param request HTTP 请求
+     * @param response HTTP 响应
+     */
+    void restoreUrl(String shortUri, ServletRequest request, ServletResponse response);
 }
