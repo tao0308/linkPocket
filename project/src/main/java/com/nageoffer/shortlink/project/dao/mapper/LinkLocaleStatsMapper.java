94023f465525ed084906241587a2af0b50029659
local git <2268952893@qq.com>
2026-08-16 18:25:04 +0800

    feature: 开发短链接监控之操作系统访问数据

--- project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/LinkLocaleStatsMapper.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/LinkLocaleStatsMapper.java
@@ -15,7 +15,7 @@ public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
      * 记录地区访问监控数据
      */
     @Insert("INSERT INTO t_link_locale_stats (full_short_url, gid, date, cnt, country, province, city, adcode, create_time, update_time, del_flag) " +
-            "VALUES (#{linkLocaleStatsDO.fullShortUrl}, #{linkLocaleStatsDO.gid}, #{linkLocaleStatsDO.date}, #{linkLocaleStatsDO.cnt}, #{linkLocaleStatsDO.country}, #{linkLocaleStatsDO.province}, #{linkLocaleStatsDO.city}, #{linkLocaleStatsDO.adcode}, NOW(), NOW(), 0) " +
-            "ON DUPLICATE KEY UPDATE cnt = cnt + #{linkLocaleStatsDO.cnt};")
+            "VALUES (#{linkLocaleStats.fullShortUrl}, #{linkLocaleStats.gid}, #{linkLocaleStats.date}, #{linkLocaleStats.cnt}, #{linkLocaleStats.country}, #{linkLocaleStats.province}, #{linkLocaleStats.city}, #{linkLocaleStats.adcode}, NOW(), NOW(), 0) " +
+            "ON DUPLICATE KEY UPDATE cnt = cnt + #{linkLocaleStats.cnt};")
     void shortLinkLocaleStats(@Param("linkLocaleStats") LinkLocaleStatsDO linkLocaleStatsDO);
 }
