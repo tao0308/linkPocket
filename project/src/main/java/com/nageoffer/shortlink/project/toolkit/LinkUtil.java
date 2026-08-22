610997afedc15e47eac977e1b297832f0c396408
local git <2268952893@qq.com>
2026-08-22 19:34:15 +0800

    feature: 跳转链接白名单

--- project/src/main/java/com/nageoffer/shortlink/project/toolkit/LinkUtil.java
+++ project/src/main/java/com/nageoffer/shortlink/project/toolkit/LinkUtil.java
@@ -7,6 +7,7 @@ import cn.hutool.core.util.StrUtil;
 import com.nageoffer.shortlink.project.common.enums.NetworkTypeEnum;
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.net.URI;
 import java.util.Date;
 import java.util.Optional;
 
@@ -154,5 +155,28 @@ public class LinkUtil {
         }
         // PC 端基本不存在蜂窝流量，统一按 WiFi 处理
         return NetworkTypeEnum.WIFI.getType();
+    }
+
+    /**
+     * 获取原始链接中的域名
+     * 如果原始链接包含 www 开头的话需要去掉
+     *
+     * @param url 创建或者修改短链接的原始链接
+     * @return 原始链接中的域名
+     */
+    public static String extractDomain(String url) {
+        String domain = null;
+        try {
+            URI uri = new URI(url);
+            String host = uri.getHost();
+            if (StrUtil.isNotBlank(host)) {
+                domain = host;
+                if (domain.startsWith("www.")) {
+                    domain = host.substring(4);
+                }
+            }
+        } catch (Exception ignored) {
+        }
+        return domain;
     }
 }
