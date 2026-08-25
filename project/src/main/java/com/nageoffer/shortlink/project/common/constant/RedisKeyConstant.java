f9a5cb6270723a8c11e5808a5d3975e9ee8f66ac
local git <2268952893@qq.com>
2026-08-25 11:05:48 +0800

    optimize: 优化短链接相关缓存 Key 规范

--- project/src/main/java/com/nageoffer/shortlink/project/common/constant/RedisKeyConstant.java
+++ project/src/main/java/com/nageoffer/shortlink/project/common/constant/RedisKeyConstant.java
@@ -10,25 +10,35 @@ public class RedisKeyConstant {
     /**
      * 短链接跳转前缀 key
      */
-    public static final String GOTO_SHORT_LINK_KEY = "short-link_goto_%s";
+    public static final String GOTO_SHORT_LINK_KEY = "short-link:goto:%s";
 
     /**
      * 短链接空值跳转前缀 key
      */
-    public static final String GOTO_IS_NULL_SHORT_LINK_KEY = "short-link_is-null_goto_%s";
+    public static final String GOTO_IS_NULL_SHORT_LINK_KEY = "short-link:is-null:goto:%s";
 
     /**
      * 短链接跳转锁前缀 key
      */
-    public static final String LOCK_GOTO_SHORT_LINK_KEY = "short-link_lock_goto_%s";
+    public static final String LOCK_GOTO_SHORT_LINK_KEY = "short-link:lock:goto:%s";
 
     /**
      * 短链接修改分组 ID 锁前缀 Key
      */
-    public static final String LOCK_GID_UPDATE_KEY = "short-link_lock_update-gid_%s";
+    public static final String LOCK_GID_UPDATE_KEY = "short-link:lock:update-gid:%s";
 
     /**
      * 短链接延迟队列消费统计 Key
      */
-    public static final String DELAY_QUEUE_STATS_KEY = "short-link_delay-queue:stats";
+    public static final String DELAY_QUEUE_STATS_KEY = "short-link:delay-queue:stats:";
+
+    /**
+     * 短链接统计 UV Key
+     */
+    public static final String SHORT_LINK_START_UV_KEY = "short-link:stats:uv:";
+
+    /**
+     * 短链接统计 UIP Key
+     */
+    public static final String SHORT_LINK_START_UIP_KEY = "short-link:stats:uip:";
 }
