6f95769468beebb3fdf1cb32ef6d84a7f2fa57b9
local git <2268952893@qq.com>
2026-08-26 17:00:20 +0800

    optimize: 修改相似原始短链接大量并发场景下生成冲突问题

--- project/src/main/java/com/nageoffer/shortlink/project/service/Impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/nageoffer/shortlink/project/service/Impl/ShortLinkServiceImpl.java
@@ -119,16 +119,9 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 .build();
         try{
             baseMapper.insert(shortLinkDO);
-
             shortLinkGotoMapper.insert(linkGotoDO);
         } catch (DuplicateKeyException ex){
-            LambdaQueryWrapper<ShortLinkDO> queryWrapper = Wrappers.lambdaQuery(ShortLinkDO.class)
-                    .eq(ShortLinkDO::getFullShortUrl, fullShortUrl);
-            ShortLinkDO hasShortLinkDO = baseMapper.selectOne(queryWrapper);
-            if(hasShortLinkDO != null){
-                log.warn("短链接: {} 重复入库",fullShortUrl);
-                throw new ServiceException("短链接生成重复");
-            }
+                throw new ServiceException(String.format("短链接生成: %s 生成重复",fullShortUrl));
         }
         stringRedisTemplate.opsForValue().set(
                 String.format(GOTO_SHORT_LINK_KEY, fullShortUrl),
@@ -565,7 +558,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 throw new ServiceException("短链接频繁生成，请稍后再试");
             }
             String originUrl = requestParam.getOriginUrl();
-            originUrl+=System.currentTimeMillis();
+            originUrl+= UUID.randomUUID().toString();
             shortUri = HashUtil.hashToBase62(originUrl);
             if(!shortUriCreateCachePenetrationBloomFilter.contains(createShortLinkDefaultDomain + "/" + shortUri)){
                 break;
