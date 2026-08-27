bb7c015b72a620aa42f85069bfabe3f63b960396
local git <2268952893@qq.com>
2026-08-27 11:48:14 +0800

    refactor: 引入网关gateway

--- admin/src/main/java/com/nageoffer/shortlink/admin/config/UserConfiguration.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/UserConfiguration.java
@@ -1,12 +1,9 @@
 package com.nageoffer.shortlink.admin.config;
 
-import com.nageoffer.shortlink.admin.common.biz.user.UserFlowRiskControlFilter;
 import com.nageoffer.shortlink.admin.common.biz.user.UserTransmitFilter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * 用户配置自动装配
@@ -18,28 +15,13 @@ public class UserConfiguration {
      * 用户信息传递过滤器
      */
     @Bean
-    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter(StringRedisTemplate stringRedisTemplate) {
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter() {
         FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new UserTransmitFilter(stringRedisTemplate));
+        registration.setFilter(new UserTransmitFilter());
         registration.addUrlPatterns("/*");
-        registration.addInitParameter("Login","/api/short-link/v1/group");
         registration.setOrder(0);
         return registration;
     }
 
-    /**
-     * 用户操作流量风控过滤器
-     */
-    @Bean
-    @ConditionalOnProperty(name = "short-link.flow-limit.enable", havingValue = "true")
-    public FilterRegistrationBean<UserFlowRiskControlFilter> globalUserFlowRiskControlFilter(
-            StringRedisTemplate stringRedisTemplate,
-            UserFlowRiskControlConfiguration userFlowRiskControlConfiguration) {
-        FilterRegistrationBean<UserFlowRiskControlFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new UserFlowRiskControlFilter(stringRedisTemplate, userFlowRiskControlConfiguration));
-        registration.addUrlPatterns("/*");
-        registration.setOrder(10);
-        return registration;
-    }
 }
 
